4ae3e3250641212926cdfaa4a3d2f679ee6f5d3e
SumitMaran2 <75078138+SumitMaran2@users.noreply.github.com>
2020-12-28 10:48:01 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,37 @@
+class Bob
+{
+	constructor(x,y,r){ 
+	
+		
+		var options={
+			isStatic:true,
+			restitution:0.6,
+			friction:0.6,
+			density:1.8
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
+		
+		World.add(world, this.body);
+		
+	}
+	display()
+	{
+		
+			var bobpos=this.body.position;		
+
+			push()
+			translate(bobpos.x, bobpos.y);
+			ellipseMode(RADIUS);
+			fill("pink")
+			ellipse(0,0,this.r, this.r);
+			pop()
+		
+			
+			
+	}
+	
+}
